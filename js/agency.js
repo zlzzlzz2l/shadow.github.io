f81d00d7d13f19ff70fbea16b4177d023baf9064
zlzzlzz2l <zlzzlzz2l@naver.com>
2020-03-29 01:16:21 +0900

    7th commit

--- js/agency.js
+++ js/agency.js
@@ -7,6 +7,7 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
+        var scrollPosition = $($(this).attr("href")).offset().top;
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
